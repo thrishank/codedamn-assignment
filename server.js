fb142ff2db7f67fc3994552b182b204f8085f7d2
thrishank <thrishankkalluru@gmail.com>
2024-01-22 04:18:45 +0530

    lab-3

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// POST Request
+
+
+
+// GET Request
+
+
+
+// PUT Request
+
+
+
+// Delete Request
+
+
+app.listen(1337, () => {"server is up and running"});
