80d06dcf20480670a551d7a33f0d14da064f6870
thrishank <thrishankkalluru@gmail.com>
2024-01-23 12:32:28 +0530

    lab-4

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
-const mongoose = require("mongoose");
 
-// connection url
-async function cn() {
-  mongoose.connect("url");
-}
 
-run();
 
-// Define schema
-const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  isCompleted: Boolean,
-});
+// import the mongoose module
 
-// Create and export the model
-const Task = mongoose.model("Task", taskSchema);
-module.exports = Task;
+
+
+
+// Connect to your mongodb cluster
+
+
+
+
+// Define the schema 
+
+
+
+// Define the model and export it as a object{}
